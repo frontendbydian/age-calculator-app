0160dbdf60b9bee9dc8a24a9b58ff8689bafc1df
dianprsty <dprasmail@gmail.com>
2024-07-03 11:31:41 +0700

    misc: project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <div className="w-screen h-screen flex justify-center items-center bg-blue-200">
-        <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-bold text-center text-blue-950">
-          React JS + Typecript + Tailwind CSS Project Starter
+      <div className="flex items-center justify-center w-screen h-screen bg-custom-off-white">
+        <h1 className="text-xl font-extrabold text-center sm:text-2xl md:text-4xl lg:text-6xl text-custom-off-black">
+          Starter Project Age Calculator
         </h1>
       </div>
+      <Footer />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        custom: {
+          purple: "hsl(259, 100%, 65%)",
+          "light-red": "hsl(0, 100%, 67%)",
+          "off-white": "hsl(0, 0%, 94%)",
+          "light-grey": "hsl(0, 0%, 86%",
+          "smokey-grey": "hsl(0, 1%, 44%)",
+          "off-black": "hsl(0, 0%, 8%)",
+        },
+      },
+      fontSize: {
+        inpus: "32px",
+      },
+    },
   },
   plugins: [],
 };
